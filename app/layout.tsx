f310b1b64b0cf73d44ddadd41dc92e935061b441
Ertugrul Hasanbeyoglu <ertugrul.hasanbeyoglu@planovo.de>
2026-02-06 16:15:18 +0100

    add GA4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import CustomCursor from "@/components/CustomCursor";
 import Navigation from "@/components/Navigation";
@@ -80,6 +81,20 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XP6ZQ2ZCFT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XP6ZQ2ZCFT');
+          `}
+        </Script>
+
         <CustomCursor />
         <AnimatedBackground />
         {children}
